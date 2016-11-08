57360dce5930d63f42ef66a60c12ac16ffc2f8bb
Josh Duffney <duffney@outlook.com>
2016-11-07 19:57:30 -0600

    update m1 demo 2 script

--- Intro-To-Regex-m1/Intro-To-Regex-m1-demos-02.ps1
+++ Intro-To-Regex-m1/Intro-To-Regex-m1-demos-02.ps1
@@ -1,28 +1,21 @@
 ﻿#region Literal_Regex_Matching
 
 #Matching a ServerName from a UNC Path
+
 '\\GFS-01\Share\regex.ps1' -match 'GFS-01'
 
 $Matches
 
-'\\ServerName\Share\regex.ps1' | Select-String -Pattern 'ServerName'
-
-$Matches
-
-#Finding .ps1 files with Where-Object
-Get-ChildItem | Where-Object Name -Match '.ps1'
-
-#Searching user's managers in Active Directory 
-Get-ADUser -Filter * -Properties Manager | Where-Object Manager -match 'smith'
-
 #Matching a user name within a distinguished name
-'CN=Administrator,CN=Users,DC=globomantics,DC=com' -match 'Administrator'
+
+'CN=jgreen,CN=Users,DC=globomantics,DC=com' -match 'jgreen'
 
 $Matches
 $Matches[0]
 
 #Determine domain name from distinguished name
-'cn=Administrator,cn=users,dc=globomantics,dc=com' -match 'globomantics,dc=com'
+
+'CN=jgreen,CN=Users,DC=globomantics,DC=com' -match 'globomantics,dc=com'
 
 $Matches[0] -replace ',dc=','.'
 
