499903a59ce2ff48fd1994c1152eb3719593eed1
Josh Duffney <duffney@outlook.com>
2016-11-10 08:04:00 -0600

    module 2 demo scripts

--- Intro-To-Regex-m2/Intro-To-Regex-m2-demos-01.ps1
+++ Intro-To-Regex-m2/Intro-To-Regex-m2-demos-01.ps1
@@ -1,17 +1,20 @@
 #region Character_Class 
 
 #Simple character class example
+
 'gray' -match 'gray'
 
 'grey' -match 'gray'
 
 'gray','grey' -match 'gr[ae]y'
 
+'gray grey' -match 'gr[ae]y'
+
 #Using select string to find multiple matches
-'gray & grey' | sls 'gr[ae]y' -all | % Matches
+
+'gray & grey' | Select-String 'gr[ae]y' -all | % Matches
 
 #Character classes to find AD cmdlets
-Get-Command -Module ActiveDirectory
 
 Get-Command -Module ActiveDirectory | Where-Object Name -Match 'Get-'
 
@@ -23,24 +26,12 @@ Get-Command -Module ActiveDirectory | Where-Object Name -Match '[GS]et-'
 (gcm -Module ActiveDirectory | Where-Object Name -Match 'Set-').count -eq `
 (gcm -Module ActiveDirectory | Where-Object Name -Match '[GS]et-').count
 
+#Character class to validate IP Address
+
+$IPAddress = (Get-NetIPAddress -InterfaceAlias Ethernet -AddressFamily IPv4).IPAddress
+
+$IPAddress
+
+$IPAddress -match '192.168.1.[0123456789][0123456789]'
+
 #endregion
-
-#region Character_Class_Ranges
-
-#Match any IP within 192.168.20.0/29
-$IPAddresses = '192.168.20.1','192.168.20.2','192.168.20.3','192.168.20.4','192.168.20.5','192.168.20.6'
-
-$IPAddresses -match '192.168.[0123456789][0123456789].[0123456789]'
-
-#Match any IP within 192.168.20.0/29 with character class ranges
-
-$IPAddresses = '192.168.20.1','192.168.20.2','192.168.20.3','192.168.20.4','192.168.20.5','192.168.20.6'
-
-$IPAddresses -match '192.168.[0-9][0-9].[0-9]'
-
-#Matching MacAddresses
-
-'svc_jenkins' -match '[a-z][a-z]'
-'' -match '[a-z][a-z]'  
-
-#endregion 
