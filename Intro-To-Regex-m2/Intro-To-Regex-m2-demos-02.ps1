31799bff7c592d5500d7a761c7a220a2f2031654
Joshua Duffney <duffney@outlook.com>
2016-11-15 06:43:36 -0600

    updated m2 d3 script

--- Intro-To-Regex-m2/Intro-To-Regex-m2-demos-02.ps1
+++ Intro-To-Regex-m2/Intro-To-Regex-m2-demos-02.ps1
@@ -1,22 +1,11 @@
 #region Character_Class_Ranges
 
 #Match any IP within 192.168.20.0/29
-
 #Vaild Host Range = 192.168.20.1 - 192.168.20.6
 
 $IPAddresses = '192.168.20.1','192.168.20.72','192.168.20.3','10.0.0.0'
 
-$IPAddresses -match '192.168.20.[123456]'
-
-#Match any IP within 192.168.20.0/29 with a character class range
-
-$IPAddresses = '192.168.20.1','192.168.20.12','192.168.20.3','10.0.0.0'
-
 $IPAddresses -match '192.168.20.[1-6]'
-
-#Negative Lookahead (Regex Magic)
-
-'192.168.20.12' -match '192.168.20.[123456](?!\d)'
 
 #Find All Users with Last Name Bailey
 Get-ADUser -Filter * | Where-Object Name -Match '[a-z]bailey' | select SurName,GivenName
