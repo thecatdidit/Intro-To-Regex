cdcaf8f9958c6d841f4486ceae253ce5b0a17a0e
Josh Duffney <duffney@outlook.com>
2016-11-21 21:30:05 -0600

    adding module 4 demos

--- Intro-To-Regex-m4/Intro-To-Regex-m4-demo-01.ps1
+++ Intro-To-Regex-m4/Intro-To-Regex-m4-demo-01.ps1
@@ -1,4 +1,27 @@
 #region Shorthand_metacharacters
 
+#Matching anything with .
+
+$matchAnything = 'a',1,'!',' '
+
+$matchAnything -match '.'
+
+#Matching word characters with \w
+$wordCharacterTest = 'a','b',' ',1,'$'
+
+$wordCharacterTest -match '\w'
+
+Get-ADUser -Filter * | Where-Object Name -Match '\wbailey'
+
+#Matching digits with \d
+
+$IPAddress = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).IPAddress
+
+$IPAddress -match '\d\d\d.\d\d\d.\d.\d\d'
+
+$IPAddress.replace('.','$') -match '\d\d\d.\d\d\d.\d.\d\d'
+
+#Escaping metacharacters
+$IPAddress.replace('.','$') -match '\d\d\d\.\d\d\d.\d\.\d\d'
 
 #endregion 
