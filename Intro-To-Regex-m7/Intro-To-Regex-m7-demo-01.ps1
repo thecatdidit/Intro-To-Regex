443de2012d2e89f9dddbb8e35207190289c10aac
Josh Duffney <duffney@outlook.com>
2016-12-11 07:28:12 -0600

    updating m7 demo 1

--- Intro-To-Regex-m7/Intro-To-Regex-m7-demo-01.ps1
+++ Intro-To-Regex-m7/Intro-To-Regex-m7-demo-01.ps1
@@ -1,9 +1,5 @@
-#Validate AD User phone number
-#Revise IP address match from module 4
-#Demonstrate option matches with ?
-
 #Star quantifer
-'expression' -match '.*'
+'expression!' -match '.*'
 
 '' -match '.*'
 
@@ -36,12 +32,3 @@ $Extension = (Get-ADUser -Identity bbailey -Properties OfficePhone).OfficePhone
 $LongDistance = (Get-ADUser -Identity gbailey -Properties OfficePhone).OfficePhone
 
 $Extension,$LongDistance -match '\d?-?\d{3}-\d{3}-\d{4}|\d{3,5}'
-
-
-$IPAddress = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).IPAddress
-
-$IPAddress -match '\d\d\d.\d\d\d.\d.\d\d'
-
-$IPAddress -match '\d+\.\d+.\d.\d+'
-
-$IPAddress -match '\d{3}\.\d{3}\.\d\.\d{1,2}'
