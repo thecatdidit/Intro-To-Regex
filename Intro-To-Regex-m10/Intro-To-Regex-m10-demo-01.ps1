112c24099b0457128aad859ab67a80ba6b05183b
Josh Duffney <duffney@outlook.com>
2017-01-20 08:03:35 -0600

    updating m10 demo scripts

--- Intro-To-Regex-m10/Intro-To-Regex-m10-demo-01.ps1
+++ Intro-To-Regex-m10/Intro-To-Regex-m10-demo-01.ps1
@@ -27,45 +27,18 @@ Enabled: true
 
 #endregion
 
-#Negative Lookbehind ?<!
+#region Negative_lookbehind
 
-[regex]::Match('duf@fney','\w+(?<!@)').value
-
-[regex]::Match('donotmatch/matchme','(?<!donotmatch\/)matchme').value
-
-[regex]::Match('master\improvement-1.2.0','(?<!master\\)[a-z]+\-([0-9]\.?)+').value
+[regex]::Match('develop\improvement-1.2.0','(?<!master\\)improvement\-1\.2\.0').value
 
 [regex]::Match('master\improvement-1.2.0','(?<!master\\)improvement\-1\.2\.0').value #has restrictions on lenght and no meatacharacters pg132
 
-[regex]::match('http://regex.info/listing.cgi?ed=3&p=all????',`
-"http://[a-z0-9]+(\.[a-z0-9]+)*\.(com|edu|info)/[-a-z0-9_:\@&?=+,.!/~*'%\$]*(?<![.,?!])").value
+#endregion
 
-[regex]::match('http://regex.info/listing.cgi?ed=3&p=all',`
-"http://[a-z0-9]+(\.[a-z0-9]+)*\.(com|edu|info)/[-a-z0-9_:\@&?=+,.!/~*'%\$]*(?<![.,?!])$").value
-
-#lookahead and lookbehind
+#region lookahead_and_lookbehind
 
 $LocalAdmins = net localgroup administrators | Out-String
 
 [regex]::Matches($localadmins,'(?s)(?<=\-\r+\n).*(?=The)').value.trim()
 
-
-(cmd /c wecutil gs appevents | Out-String) -replace '(?ns)(.*)Address:\s(?<ServerName>\w+)(.*)','$1'
-(cmd /c wecutil gs appevents | Out-String) -replace '(?s)(?:.*)Address:\s(\w+)(?:.*)','$1'
-
-
-#Lookbehind and lookahead (trim whitespaces) try multi line mode
-
-$LocalAdmins = net localgroup administrators | Out-String
-
-$regex = "(?s)(?=\-\r).*(?<!Admins)"
-
-[regex]::Match($LocalAdmins,$regex)
-
-(net localgroup administrators | Out-String) -match '(?s)(?<=\-\r).*(?=The)'
-
-#testing
-[regex]::Match($localadmins,'\-+\r+\n\w+').value
-[regex]::Match($localadmins,'\-+\r+\n\w+\r+\n\w+').value
-
-#lookbehind
+#endregion
